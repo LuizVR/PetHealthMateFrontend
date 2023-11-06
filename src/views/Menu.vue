358afccfafa274dab19e4eda61ae57e851f10b49
LuisFVR <vluiz4349@gmail.com>
2023-11-06 11:17:52 -0600

    Carpetas y clases JS Y CSS del Menu

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -22,50 +22,5 @@
       </ion-content>
     </ion-page>
   </template>
-  
-  <script>
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonCard, IonCardContent, IonIcon } from '@ionic/vue';
-  import { defineComponent } from 'vue';
-  import { paw, football, calendar, notifications } from 'ionicons/icons';
-  
-  export default defineComponent({
-    name: 'MenuPage',
-    components: {
-      IonPage,
-      IonHeader,
-      IonToolbar,
-      IonTitle,
-      IonContent,
-      IonGrid,
-      IonRow,
-      IonCol,
-      IonCard,
-      IonCardContent,
-      IonIcon
-    },
-    setup() {
-      const menuItems = [
-        { title: 'Mis mascotas', icon: paw, color: 'dark', page: '/pets' },
-        { title: 'Ejercicio', icon: football, color: 'secondary', page: '/exercise' },
-        { title: 'Calendario de la salud', icon: calendar, color: 'tertiary', page: '/health-calendar' },
-        { title: 'Campañas', icon: notifications, color: 'warning', page: '/campaigns' },
-      ];
-  
-      const openPage = (page) => {
-        // Aquí iría la lógica para abrir la página correspondiente.
-        console.log(`Opening page: ${page}`);
-      };
-  
-      return { menuItems, openPage };
-    }
-  });
-  </script>
-  
-  <style scoped>
-  ion-card {
-    --background: #fff; /* Establece el color de fondo de las tarjetas */
-  }
-  
-  /* Estilos adicionales si son necesarios */
-  </style>
-  
+  <script src="js/MenuScript.js"></script>
+  <style src="css/MenuStyles.css" scoped></style>
---
+++ src/views/js/MenuScript.js
@@ -0,0 +1,36 @@
+
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonCard, IonCardContent, IonIcon } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { paw, football, calendar, notifications } from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'MenuPage',
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardContent,
+    IonIcon
+  },
+  setup() {
+    const menuItems = [
+      { title: 'Mis mascotas', icon: paw, color: 'dark', page: '/pets' },
+      { title: 'Ejercicio', icon: football, color: 'secondary', page: '/exercise' },
+      { title: 'Calendario de la salud', icon: calendar, color: 'tertiary', page: '/health-calendar' },
+      { title: 'Campañas', icon: notifications, color: 'warning', page: '/campaigns' },
+    ];
+
+    const openPage = (page) => {
+      // Aquí iría la lógica para abrir la página correspondiente.
+      console.log(`Opening page: ${page}`);
+    };
+
+    return { menuItems, openPage };
+  }
+});
