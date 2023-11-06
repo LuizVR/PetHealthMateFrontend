40c4fb8f5ec7b1f0ef3e7b559672c58b5744f64d
LuisFVR <vluiz4349@gmail.com>
2023-11-06 10:49:32 -0600

    Cambio vite

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
   </ion-app>
 </template>
 
+
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+export default {
+  name: 'App',
+  components: {
+    IonApp,
+    IonRouterOutlet
+  }
+};
 </script>
