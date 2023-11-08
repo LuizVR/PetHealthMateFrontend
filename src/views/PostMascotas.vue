6abaf0536eb80f606d6b4336be839858d204797e
LuisFVR <vluiz4349@gmail.com>
2023-11-08 14:07:46 -0600

    Titulo

--- src/views/PostMascotas.vue
+++ src/views/PostMascotas.vue
@@ -2,7 +2,7 @@
     <ion-page>
       <ion-header>
         <ion-toolbar>
-          <ion-title>Mi amigo peludo</ion-title>
+          <ion-title>Registra a un amigo</ion-title>
         </ion-toolbar>
       </ion-header>
       <ion-content>
