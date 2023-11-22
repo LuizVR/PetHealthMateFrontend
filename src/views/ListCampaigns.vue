a93418f02e1eb4855664f4b422b7a3267aafaf65
Jairohp117 <54380167+Jairoh117@users.noreply.github.com>
2023-11-22 13:25:30 -0600

    Cambios para el rango

--- src/views/ListCampaigns.vue
+++ src/views/ListCampaigns.vue
@@ -1,25 +1,43 @@
 <template>
-    <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Mis Mascotas</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content>
-        <ion-card v-for="camp in camps" :key="camp.campaign_Id"  @click="goToPetDetails(camp.campaign_Id)">
-          <ion-card-header>
-            <img :src="camp.foto" />
-            <ion-card-title>{{ camp.titulo }}</ion-card-title>
-          </ion-card-header>
-        </ion-card>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Busca campañas cerca de tu zona</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <ion-button @click="obtenerUbicacion">Obtener Ubicación</ion-button>
+
+      <div v-if="ubicacionObtenida">
+        <ion-form class="ubicacion">
+          <ion-label class="InputUbicacion" for="rango">Ingrese el rango en kilómetros:</ion-label>
+          <input type="number" id="rango" v-model="rango" required>
+          <ion-button @click="filtrarPorRango()">Filtrar</ion-button>
+        </ion-form>
+
+        <div v-if="ubicacionesFiltradas.length > 0">
+          <h2>Campañas filtradas:</h2>
+          <ion-card v-for="camp in ubicacionesFiltradas" :key="camp.campaign_Id" @click="goToPetDetails(camp.campaign_Id)">
+            <ion-card-header>
+              <img :src="camp.foto" />
+              <ion-card-title>{{ camp.titulo }}</ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+              <p>Descripcion: {{ camp.descripcion }}</p>
+              
+            </ion-card-content>
+          </ion-card>
+        </div>
         <ion-fab class="floating-button" vertical="bottom" horizontal="end">
-          <ion-fab-button @click="goToAddPetPage">
-            <ion-icon :icon="add"></ion-icon>
-          </ion-fab-button>
-        </ion-fab>
-      </ion-content>
-    </ion-page>
-  </template>
+        <ion-fab-button @click="goToAddPetPage">
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
 
 
 <script src="./js/ListCampaignsScript.js"></script>
--- src/views/js/ListCampaignsScript.js
+++ src/views/js/ListCampaignsScript.js
@@ -1,6 +1,6 @@
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonFab, IonFabButton, IonIcon } from '@ionic/vue';
 import { add } from 'ionicons/icons';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
@@ -10,23 +10,105 @@ export default defineComponent({
   },
   data() {
     return {
+      kilometro:0,
       camps: [],
+      ubicacionObtenida: false,
+      ubicacionActual: null,
+
+      
+      rango: 0,
+      ubicacionesFiltradas: [],
       add
     };
   },
   created() {
-    this.fetchPets();
+   // this.fetchPets();
   },
   methods: {
+   
+    
     async fetchPets() {
       try {
-        const response = await axios.get('https://localhost:44329/api/Campaign'); // Reemplaza 'API_URL' con la URL de tu API
+        const response = await axios.get('https://localhost:44329/api/Campaign');
         this.camps = response.data;
-        console.log("Form Data:", this.camps);
       } catch (error) {
         console.error('Error fetching pets:', error);
       }
     },
+    
+    obtenerUbicacion() {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            this.ubicacionActual = { latitude, longitude };
+            this.ubicacionObtenida = true;
+          },
+          (error) => {
+            console.error('Error al obtener la ubicación:', error);
+          }
+        );
+      } else {
+        console.error('El navegador no soporta la geolocalización.');
+      }
+    },
+    
+    async obtenerUbicacionesEnRango(ubicacionActual, rango) {
+      if (this.camps.length > 0) {
+        const selectedFields = this.camps.map(({ latitud, longitud, foto, titulo, campaign_Id, descripcion }) => ({ lat: latitud, lon: longitud, foto, titulo,campaign_Id, descripcion }));
+        console.log(selectedFields);
+        console.log(rango);
+        console.log("Metodo obtenerUbicaciones");
+        const ubicacionesFiltradas = selectedFields.filter((ubicacion) => {
+          const distancia = this.calcularDistancia(
+            ubicacionActual.latitude,
+            ubicacionActual.longitude,
+            ubicacion.lat,
+            ubicacion.lon
+          );
+          console.log(`Distancia: ${distancia}, Rango: ${rango}`);
+          return distancia <= rango;
+        });
+    
+        this.ubicacionesFiltradas = ubicacionesFiltradas; // Modificar this.camps con las campañas filtradas
+        console.log(ubicacionesFiltradas);
+      }
+    },
+
+    async filtrarPorRango() {
+      if (this.ubicacionActual) {
+
+        console.log("estas en filtrar");
+
+
+        console.log(this.kilometro);
+
+        await this.fetchPets(); // Obtener los datos de las campañas
+        this.obtenerUbicacionesEnRango(this.ubicacionActual, this.rango); // Llamar a la función de filtrado
+      }
+    },
+    
+    calcularDistancia(lat1, lon1, lat2, lon2) {
+      // Fórmula haversine para calcular la distancia entre dos puntos en la Tierra
+      const radioTierra = 6371; // Radio medio de la Tierra en kilómetros
+      const dLat = this.degToRad(lat2 - lat1);
+      const dLon = this.degToRad(lon2 - lon1);
+      const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(this.degToRad(lat1)) *
+          Math.cos(this.degToRad(lat2)) *
+          Math.sin(dLon / 2) *
+          Math.sin(dLon / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      const distancia = radioTierra * c;
+
+      return distancia;
+    },
+    degToRad(deg) {
+      return deg * (Math.PI / 180);
+    },
+
+
     goToAddPetPage() {
       this.$router.push('/campaignsPost'); // Reemplaza 'ruta-a-tu-pagina' con la ruta a la que deseas redirigir
     },
