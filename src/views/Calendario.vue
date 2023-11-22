1ce7b4117a97303884cbb71cc016861dfd82f3a1
LuisFVR <vluiz4349@gmail.com>
2023-11-22 13:58:36 -0600

    Icono para detalle de citas

--- src/views/Calendario.vue
+++ src/views/Calendario.vue
@@ -12,9 +12,13 @@
             <ion-card-title>{{ pet.nombre }}</ion-card-title>
           </ion-card-header>
         </ion-card>
+        <ion-fab class="floating-button" vertical="bottom" horizontal="end">
+          <ion-fab-button @click="goToAddPetPage">
+            <ion-icon :icon="calendarIcon"></ion-icon>
+          </ion-fab-button>
+        </ion-fab>
       </ion-content>
     </ion-page>
   </template>
-
 
 <script src="./js/CalendarioScript.js"></script>
