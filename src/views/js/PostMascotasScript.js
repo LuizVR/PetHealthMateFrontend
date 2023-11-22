a586d616c2817eedf5bfb39e3f8fc441a81bc58c
LuisFVR <vluiz4349@gmail.com>
2023-11-22 10:24:07 -0600

    Post Mascotas con Uuid

--- src/views/js/PostMascotasScript.js
+++ src/views/js/PostMascotasScript.js
@@ -1,5 +1,6 @@
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton, IonSelect, IonSelectOption, IonToggle } from '@ionic/vue';
 import axios from 'axios';
+import { Storage } from '@capacitor/storage';
 
 export default {
   components: {
@@ -30,27 +31,40 @@ export default {
       };
       reader.readAsDataURL(event.target.files[0]);
     },
-    submitForm() {
-      // Convierte todos los valores del formulario a strings
-      const dataToSend = Object.entries(this.form).reduce((acc, [key, value]) => {
-        // Convierte todo a string, excepto la imagen que ya está en base64
-        acc[key] = key === 'foto' ? value : String(value);
-        return acc;
-      }, {});
-  
-      axios.post('https://localhost:44329/api/Pet', dataToSend, {
-        headers: {
-          'Content-Type': 'application/json'
+    async submitForm() {
+      try {
+        // Recuperar el UID del localStorage
+        const uidData = await Storage.get({ key: 'uid' });
+        const userUuid = uidData.value;
+
+        if (!userUuid) {
+          console.error('UID no encontrado');
+          return;
         }
-      })
-      .then(response => {
+
+        // Ajustar la URL de la solicitud POST
+        const apiUrl = `https://localhost:44329/api/Pet/${userUuid}`;
+
+        // Convierte todos los valores del formulario a strings
+        const dataToSend = Object.entries(this.form).reduce((acc, [key, value]) => {
+          // Convierte todo a string, excepto la imagen que ya está en base64
+          acc[key] = key === 'foto' ? value : String(value);
+          return acc;
+        }, {});
+
+        // Realiza la solicitud POST a la API con el UID en la URL
+        const response = await axios.post(apiUrl, dataToSend, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
         // Maneja la respuesta aquí
         console.log(response.data);
-      })
-      .catch(error => {
+      } catch (error) {
         // Maneja el error aquí
-        console.error(error);
-      });
+        console.error('Error al enviar el formulario:', error);
+      }
     }
   }
 };
