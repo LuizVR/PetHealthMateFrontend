f196f9d900452cdb895faea5bcbeb2113c5ce93e
LuisFVR <vluiz4349@gmail.com>
2023-11-24 07:37:52 -0600

    Vista GetCalendario

--- src/views/GetCalendario.vue
+++ src/views/GetCalendario.vue
@@ -1,18 +1,20 @@
 <template>
+    
     <ion-page>
       <ion-header>
         <ion-toolbar>
-          <ion-title>Mis Citas</ion-title>
+          <ion-title>Citas Mascotas</ion-title>
         </ion-toolbar>
       </ion-header>
       <ion-content>
-        <ion-card v-for="date in dates" :key="date.date_Id"  @click="goToPetDetails(date.date_Id)">
+        <ion-card v-for="pet in pets" :key="pet.pet_Id"  @click="goToPetDetails(pet.pet_Id)">
           <ion-card-header>
-            <ion-card-title>{{ pet.motivo }}</ion-card-title>
-            <ion-card-title>{{ pet.fecha }}</ion-card-title>
+            <img :src="pet.foto" />
+            <ion-card-title>{{ pet.nombre }}</ion-card-title>
           </ion-card-header>
         </ion-card>
       </ion-content>
     </ion-page>
   </template>
-<script src="./js/MascotasScript.js"></script>
+
+<script src="./js/CalendarioScript.js"></script>
--- src/views/js/GetCalendarioScript.js
+++ src/views/js/GetCalendarioScript.js
@@ -1,9 +1,8 @@
-// MascotasScript.js
-
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonFab, IonFabButton, IonIcon } from '@ionic/vue';
 import { add } from 'ionicons/icons';
 import { defineComponent } from 'vue';
 import axios from 'axios';
+import {Storage} from '@capacitor/storage';
 
 export default defineComponent({
   name: 'PetCard',
@@ -12,40 +11,37 @@ export default defineComponent({
   },
   data() {
     return {
-      dates: [], // Cambié el nombre de la propiedad a "dates" para reflejar mejor la estructura de los datos
-      add
+      pets: [],
+      add,
+      calendarIcon: "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' class='ionicon' viewBox='0 0 512 512'><path d='M480 128a64 64 0 00-64-64h-16V48.45c0-8.61-6.62-16-15.23-16.43A16 16 0 00368 48v16H144V48.45c0-8.61-6.62-16-15.23-16.43A16 16 0 00112 48v16H96a64 64 0 00-64 64v12a4 4 0 004 4h440a4 4 0 004-4zM32 416a64 64 0 0064 64h320a64 64 0 0064-64V179a3 3 0 00-3-3H35a3 3 0 00-3 3zm344-208a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm-80-80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm-80-80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm-80-80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24z'/></svg>",
     };
   },
   created() {
-    this.fetchDates();
+    this.fetchPets();
   },
   methods: {
-    async fetchDates() {
-      try {
-        // Realizar la solicitud GET a la URL del servicio
-        const uri = "https://localhost:44329/api/Date";
-        const response = await axios.get(uri);
+    async fetchPets() {
+      // Recuperar el uid del localStorage
+      const uidData = await Storage.get({key: 'uid'});
+      const userUuid = uidData.value;
 
-        // Verificar si la respuesta es un arreglo y no está vacío
-        if (Array.isArray(response.data) && response.data.length > 0) {
-          // Mapear los datos para adaptarlos a la estructura que esperas en tu componente
-          this.dates = response.data.map(date => ({
-            date_Id: date.fecha, // Utilizo "fecha" como identificador, ajusta esto según tus necesidades
-            pet: {
-              fecha: date.fecha,
-              motivo: date.motivo
-            }
-          }));
-        } else {
-          console.warn('La respuesta de la API no tiene el formato esperado.');
-        }
+      if (!userUuid) {
+        console.error('UID no encontrado');
+        return;
+      }
+  
+      try {
+        // Incluir el uid en la solicitud a la API
+        var uri = "https://localhost:44329/api/Pet/mismascotas/"
+        const response = await axios.get(uri+userUuid);
+        this.pets = response.data;
       } catch (error) {
-        console.error('Error fetching dates:', error);
+        console.error('Error fetching pets:', error);
       }
     },
-    goToPetDetails(dateId) {
-      // Implementa la lógica para navegar a los detalles de la mascota según el dateId
-      console.log('Navegar a los detalles de la mascota con dateId:', dateId);
+  
+    goToPetDetails(pet_Id) {
+      this.$router.push({ name: 'PostCalendario', params: { pet_Id } });
     },
   }
 });
